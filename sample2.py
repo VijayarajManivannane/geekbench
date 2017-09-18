f3caad9f1a94e617dfeab186019626ce17928f32
vijayaraj <vijayaraj.manivannane@dal.ca>
2017-09-18 17:12:19 +0000

    Another code

---
+++ sample2.py
@@ -0,0 +1,10 @@
+a=[1,2,3,4,5]
+def funt(a):
+        """takes a list and modifies it"""
+            if isinstance(a,list):
+                        print("its alist")
+                            else:
+                                        print("Give me a list")
+                                                return None
+                                            funt(a)
+                                            print(a)
